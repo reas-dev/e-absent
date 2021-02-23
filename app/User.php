ec38c1ce46522becb0e40d7f1b3d65e318b20360
andre <andreasyulianto3@gmail.com>
2021-02-23 10:36:15 +0700

    fixing conflict old model

--- app/User.php
+++ app/User.php
@@ -38,9 +38,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function vote()
-    {
-        return $this->hasOne('App\Vote');
-    }
 }
